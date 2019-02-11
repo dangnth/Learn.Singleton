5f4975d14ac7bb9dd8bca7385b5f84ae6ffa2bab
Nguyen Tran Huu Dang <pogback1f4@gmail.com>
2019-02-11 16:05:31 +0700

    Create Learn.SingletonClass.cpp

---
+++ Learn.SingletonClass.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Singleton {
+public:
+	static Singleton *GetInstance();
+private:
+	static Singleton* m_sInstance ;
+
+	Singleton() {};
+	~Singleton() {};
+};
+
+Singleton *Singleton::m_sInstance = nullptr;
+
+Singleton *Singleton::GetInstance() {
+	if (!m_sInstance)
+		m_sInstance = new Singleton;
+	return m_sInstance;
+}
+
+void main() {
+	Singleton *firstElement = Singleton::GetInstance();
+	cout << &*firstElement << endl;
+
+	Singleton *secondElement = Singleton::GetInstance();
+	cout << &*secondElement << endl;
+
+	Singleton *thirdInstance = new Singleton(*firstElement);
+	cout << &*thirdInstance << endl;
+
+	system("pause");
+}
